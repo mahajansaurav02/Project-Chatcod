47d8e40a9de336839e881fefe9a0cb5da0d3643a
mahajansaurav02 <mahajansaurav02@gmail.com>
2022-12-29 17:37:56 +0530

    add dark mode in about

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -23,10 +23,11 @@ export default function TextForm(props) {
           
     return (
         <>
-        <div className='container'>
+        <div className='container' style={{color:props.mode==="dark"?"white":"black"}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" onChange={handleOnChange} value={text} id="mybox" rows="8"></textarea>
+                <textarea className="form-control" onChange={handleOnChange} value={text} style={{backgroundColor:props.mode==="dark"?"#042743":"white",color:props.mode==="dark"?"white":"black"
+        }} id="mybox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUppercase}>Convert to Uppercase</button>
             <button type="button" className="btn btn-danger mx-2" onClick={handleLocase}>Convert to Lowercase</button>
@@ -35,12 +36,12 @@ export default function TextForm(props) {
             {/* <button className="btn btn-primary" onClick={handleUppercase}>Convert to Uppercase</button>
             <button className="btn btn-primary" onClick={handleUppercase}>Convert to Uppercase</button> */}
         </div>
-        <div className="container my-2" >
+        <div className="container my-2" style={{color:props.mode==="dark"?"white":"black"}}>
 <h1>Your text summary</h1>
 <p>{text.split(' ').length} words {text.length} characters </p>
       <p>{0.008*text.split(' ').length} Minutes to read </p>
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p>{text.length>0?text:"No text to preview"}</p>
         </div>
         </>
     )
